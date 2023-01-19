d2d21a4455d33d1b39ae021a7d578cf8ec5af2b3
KwonWonpyo <commi1106@naver.com>
2023-01-19 20:09:06 +0900

    임시로 사용했던 firebase 서버 제거 - firebase 관련된 내용 모두 제거 - .env 환경변수 제거 - node_module package에서 firebase 제외

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import PageRouter from 'components/PageRouter';
 import Header from 'components/Header';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import initApp from '../firebase';
 import 'styles/app.css';
 
 function App() {
-  const FirebaseApp = initApp; // initilize firebase server
-  const auth = getAuth(); // 로그인된 계정 정보
-
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
